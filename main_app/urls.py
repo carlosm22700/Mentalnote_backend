29585cda4b3c83df3ed7db768321940adffef6b3
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 12:55:27 -0700

    fixes url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import MoodList
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import MoodList, register, login
 
 urlpatterns = [
-    path('moodlogs/', MoodList.as_view()),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('moodlogs/', MoodList.as_view(), name='moodlogs'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
