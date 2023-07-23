c50b4c18e6fbed215b9d0c20bd58573688eaf4fa
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 12:56:42 -0700

    deletes tests.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,49 @@
 from rest_framework import generics
-from .models import User, Mood
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Mood
 from .serializers import UserSerializer, MoodSerializer
+from django.contrib.auth.models import User
 
-# Define a view for a list of User objects. This view can be used to get a list of all users or to create a new user.
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all() # Get all user objects
-    serializer_class = UserSerializer
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
+    else:
+        return Response(serializer.errors, status=400)
 
-# Define a view for a list of Mood objects. This view can be used to get a list of all users or to create a new user.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = User.objects.filter(username=username).first()
+
+    if user is None:
+        return Response({'error': 'User not found.'}, status=400)
+    if not user.check_password(password):
+        return Response({'error': 'Wrong password.'}, status=400)
+
+    refresh = RefreshToken.for_user(user)
+    return Response({
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    })
+
 class MoodList(generics.ListCreateAPIView):
-    queryset = Mood.objects.all() # Get all mood objects
     serializer_class = MoodSerializer
+
+    def get_queryset(self):
+        return Mood.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
